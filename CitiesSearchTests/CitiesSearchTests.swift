d5389b96d75bc3a74be58992231e4bed8a373398
abuzeid <abuzeid7@gmail.com>
2020-11-03 19:48:33 +0100

    Add view model search test case.

--- CitiesSearchTests/CitiesSearchTests.swift
+++ CitiesSearchTests/CitiesSearchTests.swift
@@ -6,29 +6,25 @@
 //  Copyright © 2020 abuzeid. All rights reserved.
 //
 
-import XCTest
 @testable import CitiesSearch
+import XCTest
 
-class CitiesSearchTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
+final class CitiesSearchTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let viewModel = CitiesViewModel()
+        viewModel.search(for: "A")
+        let exp = expectation(description: "searchText")
+        viewModel.citiesList.subscribe(on: .global(), {
+            XCTAssertEqual($0.count, 5)
+            exp.fulfill()
+        })
+        waitForExpectations(timeout: 0.01, handler: nil)
     }
 
     func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        measure {
+            let viewModel = CitiesViewModel()
+            viewModel.search(for: "A")
         }
     }
-
 }
